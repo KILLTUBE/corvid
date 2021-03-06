f75f48094d821f10fa8d9cd3e2ce0a39469309a1
myuce <myuce153@hotmail.com>
2021-03-07 00:45:53 +0300

    Convert 3d sky boxes properly

--- modules/MapReader.py
+++ modules/MapReader.py
@@ -1,3 +1,4 @@
+from modules.Vector3 import Vector3FromStr
 from pprint import pprint
 from typing import Mapping
 from .Side import Side
@@ -9,8 +10,21 @@ def readMap(vmf):
     entityBrushes = []
     entities = []
 
+    skyBoxBrushes = []
+    skyBoxEntityBrushes = []
+    skyBoxEntities = []
+
+    skyBoxId = -1
+    skyBoxOrigin = "0 0 0"
+    skyBoxScale = 16
+
     materials = []
     models = []
+
+    for visgroup in mapData.visgroups.visgroups:
+        if visgroup.name == "3dskybox":
+            skyBoxId = visgroup.visgroupid
+            break
 
     for solid in mapData.world.solids:
         sides = []
@@ -19,12 +33,18 @@ def readMap(vmf):
             matName = side.material.lower()
             if matName not in materials and not matName.startswith("tools/") and not matName.startswith("liquids/"):
                 materials.append(matName)
-        worldBrushes.append(Brush(sides, "world", solid.id))
+        if "visgroupid" in solid.editor and solid.editor.visgroupid == skyBoxId:
+            skyBoxBrushes.append(Brush(sides, "world", solid.id))
+        else:
+            worldBrushes.append(Brush(sides, "world", solid.id))
     
 
     for entity in mapData.entities:
         if entity.classname.startswith("prop"):
-            entities.append(entity)
+            if "visgroupid" in entity.editor and entity.editor.visgroupid == skyBoxId:
+                skyBoxEntities.append(entity)
+            else:
+                entities.append(entity)
             if "model" not in entity:
                 continue
             mdlName = entity.model.lower()
@@ -38,10 +58,17 @@ def readMap(vmf):
                     matName = side.material.lower()
                     if matName not in materials and not matName.startswith("tools/") and not matName.startswith("liquids/"):
                         materials.append(matName)
-                entityBrushes.append(Brush(sides, entity.classname, solid.id))
+                if "visgroupid" in entity.editor and entity.editor.visgroupid == skyBoxId:
+                    skyBoxEntityBrushes.append(Brush(sides, entity.classname, solid.id))
+                else:
+                    entityBrushes.append(Brush(sides, entity.classname, solid.id))
+
         elif "solid" in entity:
             if isinstance(entity.solid, str):
-                entities.append(entity)
+                if "visgroupid" in entity.editor and entity.editor.visgroupid == skyBoxId:
+                    skyBoxEntities.append(entity)
+                else:
+                    entities.append(entity)
             else:
                 sides = []
                 for side in entity.solid.sides:
@@ -49,18 +76,37 @@ def readMap(vmf):
                     matName = side.material.lower()
                     if matName not in materials and not matName.startswith("tools/") and not matName.startswith("liquids/"):
                         materials.append(matName)
-                entityBrushes.append(Brush(sides, entity.classname, solid.id))
+                if "visgroupid" in entity.editor and entity.editor.visgroupid == skyBoxId:
+                    skyBoxBrushes.append(Brush(sides, entity.classname, solid.id))
+                else:
+                    entityBrushes.append(Brush(sides, entity.classname, solid.id))
         else:
-            entities.append(entity)
-    
+            if "visgroupid" in entity.editor and entity.editor.visgroupid == skyBoxId:
+                skyBoxEntities.append(entity)
+                if entity.classname == "sky_camera":
+                    skyBoxScale = entity.scale
+                    skyBoxOrigin = Vector3FromStr(entity.origin)
+            else:
+                entities.append(entity)
+
     models = sorted(set(models))
     materials = sorted(set(materials))
+
+    print("Skybox id:", skyBoxId)
+    print("Skybox scale:", skyBoxScale)
+    print("Skybox origin:", skyBoxOrigin)
 
     return {
         "worldBrushes": worldBrushes,
         "entityBrushes": entityBrushes,
         "entities": entities,
+        "skyBoxId": skyBoxId,
+        "skyBoxScale": float(skyBoxScale),
+        "skyBoxOrigin": skyBoxOrigin,
+        "skyBoxBrushes": skyBoxBrushes,
+        "skyBoxEntities": skyBoxEntities,
+        "skyBoxEntityBrushes": skyBoxEntityBrushes,
         "materials": materials,
         "models": models,
-        "sky": mapData.world.skyname.lower() if "skyname" in mapData.world else "sky"
+        "skyName": mapData.world.skyname.lower() if "skyname" in mapData.world else "sky"
     }
