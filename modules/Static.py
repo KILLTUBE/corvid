cc7180fc52b6c462d48d8f7910541bd601aa6de0
myuce <myuce153@hotmail.com>
2021-03-24 23:08:25 +0300

    More fixes for VMT files with bad syntax

--- modules/Static.py
+++ modules/Static.py
@@ -45,10 +45,18 @@ def uniqueName(name: str):
 
 # some vmt files are written so badly, we have to fix them make sure they will be parsed correctly
 def fixVmt(vmt: str):
-    result = "";
-    lines = vmt.replace("\t", " ").replace("\\", "/").replace(".vtf", "").split("\n");
+    result = ""
+    result2 = ""
+    lines = vmt.replace("\t", " ").replace("\\", "/").replace(".vtf", "").split("\n")
     for line in lines:
+        res2 = ""
         line = line.replace('"', " ").strip().lower()
+        if line.startswith("{") and line != "{":
+            line = line[1:]
+            result += "{\n"
+        if line.endswith("}") and line != "}" and "{" not in line:
+            line = line[:-1]
+            res2 = "}\n"
         if len(line) == 0 or line.startswith("/"):
             continue
         line = " ".join(line.split())
@@ -59,7 +67,7 @@ def fixVmt(vmt: str):
         key = tok[0]
         value = " ".join(tok[1:])
         line = f'"{key}" "{value}"'
-        result += line + "\n"
+        result += line + "\n" + res2
     return result;
 
 def rgbToHex(rgb):
