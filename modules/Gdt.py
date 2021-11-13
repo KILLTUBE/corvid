e6232118fa4b017dfe9f28143a36a9057e432cce
Mehmet <myuce153@hotmail.com>
2021-11-13 18:28:33 +0300

    Remove comments from GDT files

--- modules/Gdt.py
+++ modules/Gdt.py
@@ -2,19 +2,16 @@ class Gdt:
     def __init__(self, entries={}):
         self.entries = entries
     
-    def add(self, name: str, type: str, data: dict, comment: str = ""):
+    def add(self, name: str, type: str, data: dict):
         self.entries[name] = {
             "name": name,
             "type": type,
             "data": data,
-            "comment": comment
         }
     
     def toStr(self):
         res = "{\n"
         for entry in list(self.entries.values()):
-            if entry["comment"] != "":
-                res += f' // {entry["comment"]}\n'
             res += f' "{entry["name"]}" ( "{entry["type"]}.gdf" )\n'
             res += " {\n"
             for key, value in entry["data"].items():
