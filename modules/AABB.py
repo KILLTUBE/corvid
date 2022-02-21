e2c56277f02884053d0651e436bef2f489f240f9
Mehmet <myuce153@hotmail.com>
2022-02-22 00:19:05 +0300

    Update AABB.py

--- modules/AABB.py
+++ modules/AABB.py
@@ -3,10 +3,18 @@ from .Vector3 import Vector3
 class AABB:
     min: Vector3
     max: Vector3
+    top: Vector3
+    forward: Vector3
+    right: Vector3
+    center: Vector3
 
     def __init__(self, _min: Vector3 = Vector3.Zero(), _max: Vector3 = Vector3.Zero()) -> 'AABB':
         self.min = _min
         self.max = _max
+        self.center = _min.lerp(max, 0.5)
+        self.top = self.center + Vector3(0, 0, (_max.z - _min.z) / 2)
+        self.forward = self.center + Vector3(0, 0, (_max.x - _min.x) / 2)
+        self.right = self.center + Vector3(0, 0, (_max.y - _min.y) / 2)
     
     def update(self, new):
         self.min = self.min.min(new)
