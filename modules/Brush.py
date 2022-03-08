22105f0201f6ba5e65ecd9861ee7f9c19c2f1373
Mehmet <myuce153@hotmail.com>
2022-03-08 21:23:46 +0300

    Create AABB for brushes

--- modules/Brush.py
+++ modules/Brush.py
@@ -3,7 +3,7 @@ from .Side import Side
 from .AABB import AABB
 from math import isnan
 
-def getPlaneIntersectıon(side1: Side, side2: Side, side3: Side) -> Vector3:
+def getPlaneIntersection(side1: Side, side2: Side, side3: Side) -> Vector3:
     normal1: Vector3 = side1.normal().normalize()
     normal2: Vector3 = side2.normal().normalize()
     normal3: Vector3 = side3.normal().normalize()
@@ -39,6 +39,8 @@ class Brush:
         self.getIntersectionPoints()
         self.entity = entity
         self.entData = entData
+        self.isToolBrush = False
+        self._box: AABB = None
 
     def getIntersectionPoints(self):
         n = len(self.sides)
@@ -46,7 +48,7 @@ class Brush:
             for j in range(n - 1):
                 for k in range(n):
                     if i != j and i != k and j != k:
-                        intersectionPoint: Vector3 = getPlaneIntersectıon(
+                        intersectionPoint: Vector3 = getPlaneIntersection(
                             self.sides[i], self.sides[j], self.sides[k]
                         )
                         if intersectionPoint is not None and intersectionPoint.isLegal(self.sides):
@@ -59,13 +61,44 @@ class Brush:
                 self.sides[i].sortVertices()
             if self.sides[i].hasDisp:
                 self.hasDisp = True
+            if not self.sides[i].material.startswith("tools"):
+                self.isToolBrush = True
 
     # get the bounding box of a brush
-    def getAABB(self):
-        res = AABB()
+    def AABB(self):
+        if self._box is not None:
+            return self._box
+
+        _min = Vector3.Zero()
+        _max = Vector3.Zero()
 
         for side in self.sides:
             for point in side.points:
-                res.update(point)
+                _min.set(_min.min(point))
+                _max.set(_max.max(point))
+        
+        self._box = AABB(_min, _max)
+        return self._box
+    
+    def GetDecalPoints(self, box: 'AABB'):
+        # decals don't work on displacements
+        if self.hasDisp:
+            return None
+
+        if not self.AABB().IsTouching(box):
+            return None
+        
+        res: list[Vector3] = []
+        points: list[Vector3] = []
+
+        for side in self.sides:
+            if side.IsTouching(box):
+                if side.normal().normalize().dot(box.center - side.p1) < 0: # if it's behind the side, we don't need it
+                    continue
+                points.append(side.getClosestPoint(box.center))
+        
+        for point in points:
+            if point.isLegal(self.sides):
+                res.append(point)
         
         return res
