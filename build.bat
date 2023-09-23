28d96a69669f7f8c43551d1769ab3981214f98bd
Mehmet <myuce153@hotmail.com>
2023-09-24 00:58:56 +0300

    Rename app.py to main.py

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 
 del dist /S /F /Q
-pyinstaller app.py -F -i res/icon.ico -n "Corvid" --windowed --add-data "SourceIO/source1/vtf/VTFWrapper/VTFLib.x64.dll;."
+pyinstaller main.py -F -i res/icon.ico -n "Corvid" --windowed --add-data "SourceIO/source1/vtf/VTFWrapper/VTFLib.x64.dll;."
 @mkdir "./dist/res"
 xcopy /s "./res" "./dist/res"
 copy .\SourceIO\source1\vtf\VTFWrapper\VTFLib.x64.dll .\dist\res\
