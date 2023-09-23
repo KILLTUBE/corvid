99eff4fa3492d0856f523e66a6e6fe2c62886735
Mehmet <myuce153@hotmail.com>
2023-09-23 23:45:36 +0300

    Refactor

--- Formats/Common/CoDUV.py
+++ Formats/Common/CoDUV.py
@@ -2,6 +2,11 @@ from mathutils import Vector
 from .StandardUV import StandardUV
 
 class CoDUV(StandardUV):
+    __slots__ = ("lxScale", "lyScale")
+
+    lxScale: float
+    lyScale: float
+
     def GetUV(self, vertex: Vector, normal: Vector, texSize: Vector = None) -> Vector:
         texSize = Vector((512, 512))
         return super().GetUV(vertex, normal, texSize)
